992dbc3b6b498e0ae55c2e668c3f23949942c50b
max-luminance <181053430+max-luminance@users.noreply.github.com>
2024-08-22 14:24:22 +0800

    initialize sets state

--- programs/solana-options/src/instructions/initialize.rs
+++ programs/solana-options/src/instructions/initialize.rs
@@ -1,9 +1,26 @@
 use anchor_lang::prelude::*;
 
+use crate::state::CoveredCall;
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub seller: Signer<'info>,
+    #[account(
+        init, 
+        payer = seller,
+        space = 8 + CoveredCall::INIT_SPACE,
+        seeds = [b"covered-call"],
+        bump,
+    )]
+    pub data: Account<'info, CoveredCall>,
+    pub system_program: Program<'info, System>,
+}
 
 pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {{:?}}", ctx.program_id);
+    msg!("Greetings from: {:?}", ctx.accounts.seller.key());
+    ctx.accounts.data.set_inner(CoveredCall {
+        seller: ctx.accounts.seller.key()
+    });
     Ok(())
 }
--- tests/solana-options.ts
+++ tests/solana-options.ts
@@ -1,16 +1,35 @@
+import { expect } from "chai";
+import { startAnchor } from "solana-bankrun";
+import { BankrunProvider } from "anchor-bankrun";
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaOptions } from "../target/types/solana_options";
+const IDL = require("../target/idl/solana_options.json");
 
+// TODO:- Transfer mint to the program
 describe("solana-options", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
-  const program = anchor.workspace.SolanaOptions as Program<SolanaOptions>;
-
   it("Is initialized!", async () => {
+    const context = await startAnchor(".", [], []);
+    const provider = new BankrunProvider(context);
+
+    // Configure the client to use the local cluster.
+    // anchor.setProvider(anchor.AnchorProvider.env());
+    const program = new Program<SolanaOptions>(IDL, provider);
+
+    // const program = anchor.workspace.SolanaOptions as Program<SolanaOptions>;
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
+
+    // Check state
+    const [addr] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("covered-call")],
+      program.programId
+    );
+
+    const state = await program.account.coveredCall.fetch(addr);
+    expect(state).to.deep.equal({
+      seller: context.payer.publicKey,
+    });
   });
 });
